9b8cce041dcdf8eb59dea4a6ec23e29cfaf4722a
Rayquane <78152375+Rayquane@users.noreply.github.com>
2022-04-01 16:31:55 +0200

    Create ligne-commande.ts

---
+++ src/app/classes/ligne-commande.ts
@@ -0,0 +1,14 @@
+// la classe contient les informations retournées par une requête.
+
+export class LigneCommande {
+    constructor(
+        public id: number,
+        public nomPlateau: String,
+        public quantite: number,
+        public prix: number
+        
+    )
+    {}
+   
+}
+
